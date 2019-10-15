20089cf87b39353cd4d2ad2ef11e05b072cd3204
milomacphail <milomacphail@gmail.com>
2019-10-14 20:48:13 -0400

    Fixed type error.

--- Utilities.cs
+++ Utilities.cs
@@ -4,13 +4,17 @@ using System.Text;
 
 namespace CSharp_Generics
 {
-    public class Utilities
-    {
+    public class Utilities<T> where T : IComparable, new()    {
         public int Max(int a, int b)
         {
             return a > b ? a : b;
         }
 
+        public void DoSomething(T Value)
+        {
+            var obj = new T();
+        }
+         
         public T Max <T>(T a, T b) where T : IComparable
         {
             return a.CompareTo(b) > 0 ? a : b;
